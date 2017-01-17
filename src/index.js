38d2149d799e26ba79218cf1d0d6024a26ba9acd
Mattia46 <mattia.assogna@gmail.com>
2017-01-17 14:23:42 +0000

    added webpack config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render(){
+    return(
+      <p>Hello</p>
+    )
+  }
+}
+
+ReactDOM.render (
+  <App />,
+  document.getElementById('app')
+)
