242ee9ab7a36aa653ee3fb9a7a76dbb27a751706
Mattia Assogna <mattia.assogna@gmail.com>
2017-01-19 18:43:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import Main from './components/Main/Main';
 
 
-class App extends Component {
-  render(){
-    return(
-      <p>Hello</p>
-    )
-  }
-}
-
 ReactDOM.render (
   <Main />,
   document.getElementById('app')
